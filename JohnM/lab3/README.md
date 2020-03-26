32b1ee34c5d2ede4fe30819e47f3811cb6862ad4
John MacTavish <john@techsolx.com>
2020-03-25 18:10:43 -0700

    Add lab3 8Ball,
    Use choice method
    add README.md and Dockerfile.
    
    Reorder folders add labs
    Run black

---
+++ JohnM/lab3/README.md
@@ -0,0 +1,12 @@
+# Simple python 8Ball game
+
+### run this with
+
+```python3 app.py```
+
+### it can also be run in a docker container
+
+```
+docker build --no-cache --tag 8ball:latest .
+docker run -it 8ball
+```
---
+++ JohnM/lab3/app.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import random
+import time
+
+responses = [
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful.",
+]
+
+
+def ask_question():
+    print(f"\nPlay magic 8Ball or type 'quit()' to quit.")
+    question = input("Please ask a question? ")
+    if question == "quit()":
+        quit()
+    elif "?" in question:
+        answer = random.choice(responses)
+        time.sleep(random.randint(1, 3))
+        print(f"\n{answer}\n")
+    else:
+        print(
+            f"'{question}' <-- doesn't look like a question, "
+            f"it looks like a statement? \n"
+        )
+        ask_question()
+
+
+while True:
+    ask_question()
